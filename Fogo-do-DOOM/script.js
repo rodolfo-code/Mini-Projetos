9c457999bba7872c555442c96fab262ef546298a
rodolfo_oliveira <rod.vgo@gmail.com>
2020-12-29 06:19:24 -0300

    estrutura inicial

--- Fogo-do-DOOM/script.js
+++ Fogo-do-DOOM/script.js
@@ -1,17 +1,19 @@
-const firePixelsArray = []
-const fireWidth = 2
-const fireHeight = 3
+const firePixelsArray = [];
+const fireWidth = 2;
+const fireHeight = 3;
 
 function start() {
-
+  createFireDataStructure();
+  console.log(firePixelsArray);
 }
 
 
+
 function createFireDataStructure() {
-  const numberOfPixels = fireWidth * fireHeight
+  const numberOfPixels = fireWidth * fireHeight;
 
   for (let i = 0; i < numberOfPixels; i++) {
-    firePixelsArray[i] = 0
+    firePixelsArray[i] = 0;
   }
 }
 
@@ -23,4 +25,4 @@ function renderFire() {
 
 }
 
-start()
+start();
