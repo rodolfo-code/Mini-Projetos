b2e08dd3b0e4de522a57aa4d933f50411ee4c002
rodolfo_oliveira <rod.vgo@gmail.com>
2020-12-29 15:37:40 -0300

    linhas e colunas

--- Fogo-do-DOOM/script.js
+++ Fogo-do-DOOM/script.js
@@ -4,7 +4,7 @@ const fireHeight = 3;
 
 function start() {
   createFireDataStructure();
-  console.log(firePixelsArray);
+  renderFire()
 }
 
 
@@ -22,7 +22,24 @@ function calculateFirePropagation() {
 }
 
 function renderFire() {
+  let html = '<table cellpadding=0 cellspacing=0>'
 
+  for (let row = 0; row < fireHeight; row++) {
+    html += '<tr>'
+
+    for (let column = 0; column < fireWidth; column++) {
+      const pixelIndex = column + (fireWidth * row)
+      html += '<td>'
+      html += pixelIndex
+      html += '</td>'
+    }
+
+    html += '</tr>'
+  }
+
+  html += '</table>'
+
+  document.querySelector('#fireCanvas').innerHTML = html
 }
 
 start();
