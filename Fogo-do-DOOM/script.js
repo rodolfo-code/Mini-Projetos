f2b8cf9b171840c3650b96d22d762fa17d2105c5
rodolfo_oliveira <rod.vgo@gmail.com>
2020-12-29 06:13:57 -0300

    inicio do projeto

---
+++ Fogo-do-DOOM/script.js
@@ -0,0 +1,26 @@
+const firePixelsArray = []
+const fireWidth = 2
+const fireHeight = 3
+
+function start() {
+
+}
+
+
+function createFireDataStructure() {
+  const numberOfPixels = fireWidth * fireHeight
+
+  for (let i = 0; i < numberOfPixels; i++) {
+    firePixelsArray[i] = 0
+  }
+}
+
+function calculateFirePropagation() {
+
+}
+
+function renderFire() {
+
+}
+
+start()
