28107d15e01c31baf387ac2c1cd08f480974237f
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-12 01:02:22 -0300

    press enter

--- conversor-bin-dec/script.js
+++ conversor-bin-dec/script.js
@@ -1,11 +1,41 @@
-const bin = '1000';
-const convert = parseInt('bin', 2)
 
-console.log(convert)
+// if(!input) {
+//   document.querySelector('.decimal').innerHTML = ''
+// }
 
 const converter = () => {
   document.querySelector('.button')
   .addEventListener('click', () => {
-    document
+    const input = document.querySelector('.binary')
+    const convert = () => parseInt(input.value, 2);
+    if(input.value.split('').some(element => element != '1' && element != '0')) {
+      alert('Invalid input: enter a binary number!')
+    } else {
+      document.querySelector('.decimal').innerHTML = convert()
+    }
+    
   })
 }
+
+
+// const input = document.querySelector(".binary").value;
+// const pressConvert = () => {
+//   const convert = () => parseInt(input, 2);
+//   const inputEnter = document.querySelector(".binary");
+//   inputEnter.addEventListener('keypress', (e) => {
+//     if (e.which == 13 && input.split("").some((element) => element != "1" && element != "0")) {
+//       alert("Invalid input: enter a binary number!");
+//     } else {
+//       document.querySelector(".decimal").innerHTML = convert();
+//     }
+//   })
+// };
+
+converter();
+// pressConvert()
+
+
+
+
+
+    
