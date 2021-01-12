16c104265ca67e6e9b217142260fe678d03cd7db
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-12 01:03:25 -0300

    ajustes

--- conversor-bin-dec/script.js
+++ conversor-bin-dec/script.js
@@ -18,18 +18,19 @@ const converter = () => {
 }
 
 
-// const input = document.querySelector(".binary").value;
-// const pressConvert = () => {
-//   const convert = () => parseInt(input, 2);
-//   const inputEnter = document.querySelector(".binary");
-//   inputEnter.addEventListener('keypress', (e) => {
-//     if (e.which == 13 && input.split("").some((element) => element != "1" && element != "0")) {
-//       alert("Invalid input: enter a binary number!");
-//     } else {
-//       document.querySelector(".decimal").innerHTML = convert();
-//     }
-//   })
-// };
+const input = document.querySelector(".binary");
+
+const pressConvert = () => {
+  const convert = () => parseInt(input, 2);
+  const inputEnter = document.querySelector(".binary");
+  inputEnter.addEventListener('keypress', (e) => {
+    if (e.which == 13 && input.split("").some((element) => element != "1" && element != "0")) {
+      alert("Invalid input: enter a binary number!");
+    } else {
+      document.querySelector(".decimal").innerHTML = convert();
+    }
+  })
+};
 
 converter();
 // pressConvert()
