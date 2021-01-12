63867c3079c408491da5febccb19452b6dc75c9b
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-12 01:05:02 -0300

    desfaz evento de teclado

--- conversor-bin-dec/script.js
+++ conversor-bin-dec/script.js
@@ -1,8 +1,3 @@
-
-// if(!input) {
-//   document.querySelector('.decimal').innerHTML = ''
-// }
-
 const converter = () => {
   document.querySelector('.button')
   .addEventListener('click', () => {
@@ -17,23 +12,7 @@ const converter = () => {
   })
 }
 
-
-const input = document.querySelector(".binary");
-
-const pressConvert = () => {
-  const convert = () => parseInt(input, 2);
-  const inputEnter = document.querySelector(".binary");
-  inputEnter.addEventListener('keypress', (e) => {
-    if (e.which == 13 && input.split("").some((element) => element != "1" && element != "0")) {
-      alert("Invalid input: enter a binary number!");
-    } else {
-      document.querySelector(".decimal").innerHTML = convert();
-    }
-  })
-};
-
 converter();
-// pressConvert()
 
 
 
