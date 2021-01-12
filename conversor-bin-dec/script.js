4bba27cddf205ebf47cc67e84fe315d3a92ef6d4
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-11 23:01:37 -0300

    arquivos iniciais

---
+++ conversor-bin-dec/script.js
@@ -0,0 +1,11 @@
+const bin = '1000';
+const convert = parseInt('bin', 2)
+
+console.log(convert)
+
+const converter = () => {
+  document.querySelector('.button')
+  .addEventListener('click', () => {
+    document
+  })
+}
