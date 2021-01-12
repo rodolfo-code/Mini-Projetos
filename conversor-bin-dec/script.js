792d9467858f48ac24775811245acd1e1ad4e915
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-12 01:09:12 -0300

    script final

--- conversor-bin-dec/script.js
+++ conversor-bin-dec/script.js
@@ -1,21 +1,14 @@
 const converter = () => {
-  document.querySelector('.button')
-  .addEventListener('click', () => {
-    const input = document.querySelector('.binary')
+  document.querySelector(".button").addEventListener("click", () => {
+    const input = document.querySelector(".binary");
     const convert = () => parseInt(input.value, 2);
-    if(input.value.split('').some(element => element != '1' && element != '0')) {
-      alert('Invalid input: enter a binary number!')
+    if (input.value.split("").some((element) => element != "1" && element != "0")
+    ) {
+      alert("Invalid input: enter a binary number!");
     } else {
-      document.querySelector('.decimal').innerHTML = convert()
+      document.querySelector(".decimal").innerHTML = convert();
     }
-    
-  })
-}
+  });
+};
 
 converter();
-
-
-
-
-
-    
